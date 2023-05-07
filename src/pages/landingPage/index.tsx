6f0aabe7c657222e37322b6dca286074b330ed91
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2023-05-07 09:46:07 -0300

    Update index.tsx

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./styles.scss"
 import { Row, Col } from 'react-styled-flexboxgrid'
 
 const LandingPage: React.FC = () => {
+
+  
   // const navigate = useNavigate()
 
-  // const connect = async () => {
-  //   // Connect to wallet with await
-  //   return 'connected'
-  // }
+  const connect = async () => {
+    // Connect to wallet with await
+    if (window.ethereum) {
+      // Solicita autorização para acessar a conta do usuário
+      window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then()
+        .catch(console.error);
+    } else {
+      console.error('instalar extensão metamask!!');
+    }
+  }
 
   // const subscribe = async () => {
   //   return 'subscribed'
   // }
 
   const handle: any = async () => {
-    // call wallet function with await
+    connect()
 
     // call purchase subscription function with await
     return 'task finished'
@@ -28,7 +37,7 @@ const LandingPage: React.FC = () => {
       </Row>
       
       <Row middle='md' center='md'>
-        <button className='button' onClick={(() => handle)}>Start</button>
+        <button className='button' onClick={(() => handle())}>Start</button>
       </Row>
 
       <Row center='md' middle='md' >
@@ -50,7 +59,7 @@ const LandingPage: React.FC = () => {
         </Col>
         <Col md={3} className='benefits-container'>
           <p>
-            <b>AAnálise Quantitativa</b>
+            <b>Análise Quantitativa</b>
           </p>
           <p>
             Através da análise dos dados processados pela inteligência artificial, o sistema produz uma avaliação do criptoativo em questão.
